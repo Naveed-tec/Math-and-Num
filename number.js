f447f7a7b2d582c44f03b7b81cadbd2466c10357
Muhammad Naveed <mu42ghal.33@gmail.com>
2024-10-25 19:57:38 +0500

    Add files via upload
    File containing Math methods

---
+++ number.js
@@ -0,0 +1,29 @@
+// const score = 400;
+// console.log(typeof score)
+
+// const num = new Number(100)
+// console.log(num.toString().length)
+// console.log(num.toFixed(2))
+
+// const newNum = 233.84;
+// console.log(newNum.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//     ------Math--------
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.1))
+// console.log(Math.min(4,5,2,3,1))
+// console.log(Math.max(4,5,2,3,1))
+
+// console.log(Math.ceil(Math.random()*6))
+// console.log(Math.ceil(Math.random()*10) && Math.ceil(Math.random()*20))
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
